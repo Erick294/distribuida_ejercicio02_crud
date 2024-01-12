9b57b1901eb2c438ed24066cf809647914583d0a
Erick294 <85328371+Erick294@users.noreply.github.com>
2024-01-12 08:16:03 -0500

    Update Principal.java

--- examen01-correccion/src/main/java/com/distribuida/Principal.java
+++ examen01-correccion/src/main/java/com/distribuida/Principal.java
@@ -7,6 +7,9 @@ import jakarta.enterprise.inject.se.SeContainer;
 import jakarta.enterprise.inject.se.SeContainerInitializer;
 
 import static spark.Spark.*;
+import spark.Request;
+import spark.Response;
+import spark.Spark;
 
 public class Principal {
     static SeContainer container;
@@ -31,5 +34,27 @@ public class Principal {
         post("/books", bookRest::insert, mapper::writeValueAsString);
         put("/books/:id", bookRest::update, mapper::writeValueAsString);
         delete("/books/:id", bookRest::remove, mapper::writeValueAsString);
+
+        // Configuración CORS
+        options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        Spark.before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+            response.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.header("Access-Control-Allow-Headers", "*");
+            response.type("application/json");
+        });
     }
 }
